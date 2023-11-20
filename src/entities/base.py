760faa5c193724bed7249bc9cec2276acefb0c66
Maestr0n <arm.ale.ave@gmal.com>
2023-11-21 00:34:11 +0400

    almost done

--- src/entities/base.py
+++ src/entities/base.py
@@ -4,23 +4,44 @@ import pygame
 class Entity:
     x: float
     y: float
-    move: int = 5
+    move: int = 1/2
     image_path: str
 
     def __init__(self, image_path: str, x: float, y: float):
         self._image_path = image_path
         self.image = pygame.image.load(self._image_path)
-        self.x = x
-        self.y = y
+        self.x = x - 24
+        self.y = y - 24
 
     def go_left(self):
-        self.x -= self.move
+        if self.x > -24:
+            self.x -= self.move
+            if self.x == -24:
+                self.x = 276
 
     def go_right(self):
-        self.x += self.move
-
-    def go_up(self):
-        self.y += self.move
+        if self.x < 276:
+            self.x += self.move
+            if self.x == 276:
+                self.x = -24
 
     def go_down(self):
-        self.y -= self.move
+        if self.y < 276:
+            self.y += self.move
+            if self.y == 276:
+                self.y = -24
+
+    def go_up(self):
+        if self.y > -24:
+            self.y -= self.move
+            if self.y == - 24:
+                self.y = 276
+
+    @property
+    def cord(self) -> tuple:
+        return self.x, self.y
+
+    @cord.setter
+    def cord(self, new_cord):
+        self.x = new_cord[0]
+        self.y = new_cord[1]
